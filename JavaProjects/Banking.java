fd23a88822daed144b48ee91da34537078a4abea
Rishab Das <das.rishabk@gmail.com>
2022-06-22 15:11:47 -0400

    created outlines for options

--- JavaProjects/Banking.java
+++ JavaProjects/Banking.java
@@ -9,9 +9,22 @@ public class Banking {
         System.out.println("Welcome to Rishab's Bank");
         System.out.println("Are you here to open account, withdraw, or deposit  (a/b/c)?");
 
-        char decision = scan.nextLine();
+        String decision = scan.nextLine();
 
-        
+        if (decision.equals("a")) {
+            // Opening account
+            System.out.println("Great, let's get started openning your account");
+
+        } else if (decision.equals("b")) {
+            // Withdraw 
+            System.out.println("You want to withdraw, great, let's get started");
+
+        } else {
+            // Deposit
+            System.out.println("You want to deposit, great, let's get started");
+        }
+
+
 
 
     }
