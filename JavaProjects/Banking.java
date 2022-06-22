7414e6f0ffdb016be4a434e800d2770ead71621f
Rishab Das <das.rishabk@gmail.com>
2022-06-22 15:04:59 -0400

    created scaner and askd first question

--- JavaProjects/Banking.java
+++ JavaProjects/Banking.java
@@ -1,2 +1,19 @@
-package Java-Projects;
+package JavaProjects;
+
+import java.util.Scanner;
+
+public class Banking {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to Rishab's Bank");
+        System.out.println("Are you here to open account, withdraw, or deposit  (a/b/c)?");
+
+        char decision = scan.nextLine();
+
+        
+
+
+    }
+}
 
