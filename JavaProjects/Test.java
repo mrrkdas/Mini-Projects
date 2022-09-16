ac5ea0895b3bbe072a39b162aeb1cc30ad332bf7
Rishab Das <das.rishabk@gmail.com>
2022-09-16 19:16:40 -0400

    trying to load a csv file

--- JavaProjects/Test.java
+++ JavaProjects/Test.java
@@ -1,6 +1,12 @@
-i
+import weka.core.converters.ConverterUtils.DataSource;
 
 public class Test {
-    @Test
+    public static void main(String[] args) {
+        try {
+            DataSource source = new DataSource("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
