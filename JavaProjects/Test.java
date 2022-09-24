e8aa4b651a641d7d0bf816a026406aafd5853260
Rishab Das <das.rishabk@gmail.com>
2022-09-24 18:29:30 -0400

    trying to use C++ and C in vscode

--- JavaProjects/Test.java
+++ JavaProjects/Test.java
@@ -1,17 +1,18 @@
 import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 import weka.experiment.InstanceQuery;
 
-// public class Test {
-//     public static void main(String[] args) throws Exception {
-//         DataSource source = new DataSource("/Users/rish/Desktop/Mini-Projects/JavaProjects/heartdiseasedataset.csv");
-//         Instances data = source.getDataSet();
+public class Test {
+    public static void main(String[] args) throws Exception {
+        DataSource source = new DataSource("/Users/rish/Desktop/Mini-Projects/JavaProjects/heartdiseasedataset.csv");
+        Instances data = source.getDataSet();
 
-//         if (data.classIndex() == -1) {
-//             data.setClassIndex(data.numAttributes() - 1);
-//         }
+        if (data.classIndex() == -1) {
+            data.setClassIndex(data.numAttributes() - 1);
+        }
        
 
         
-//     }
+    }
 
-// }
+}
